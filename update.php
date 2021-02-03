63114fe260ed73a822e1104686e0c9ba0f84d0ec
Timo Anttila <14306628+timoanttila@users.noreply.github.com>
2021-02-03 09:32:54 +0200

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,9 @@
 <?php
+// Open connection to database
+$db = new mysqli("localhost", "user", "password", "database");
+if ($db->connect_error) { die("Connection failed: " . $db->connect_error); }
+mysqli_set_charset($db,"utf8");
+
 // If submit
 if($_POST["download"){
 
